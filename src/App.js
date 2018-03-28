dbed0f76fe5790147732ea0c86dbc4674a7544bd
Brian Davenport <bdav87@gmail.com>
2018-03-28 08:55:32 -0500

    First commit of barebones react todo list

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import './App.css';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import uuid from 'uuid';
+import $ from 'jquery';
+import Todos from './Components/Todos';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: [],
+      todos: []
+    }
+  }
+
+  getTodos(){
+    $.ajax({
+      url: 'https://jsonplaceholder.typicode.com/todos',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({todos: data}, function(){
+          console.log(this.state);
+        })
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+      }
+    })
+  }
+
+  getProjects(){
+
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        id: uuid.v4(),
+        title: 'Business Site',
+        category: 'Web Design'
+      },
+      {
+        id: uuid.v4(),
+        title: 'Social app',
+        category: 'Mobile Dev'
+      },
+      {
+        id: uuid.v4(),
+        title: 'Ecommerce shopping cart',
+        category: 'Web Dev'
+      }
+    ]});
+    this.getTodos();
+    this.getProjects();
+  }
+
+  componentDidMount(){
+    this.getTodos();
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects: projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects: projects});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+        <hr />
+        <Todos todos={this.state.todos}/> 
+      </div>
+    );
+  }
+}
+
+export default App;
